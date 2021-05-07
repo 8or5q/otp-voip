5446cdf866da64bb3280df531749f347f5aef447
8or5q <76275385+8or5q@users.noreply.github.com>
2021-05-07 17:31:33 +0900

    fixes for build_deb

--- build_deb.sh
+++ build_deb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ./otp-voip_1.0.0_all/usr/bin/
 cp ./shares/otp_voip_* ./otp-voip_1.0.0_all/usr/bin/
 dpkg-deb --build ./otp-voip_1.0.0_all ./build/otp-voip_1.0.0_all.deb
+rm ./otp-voip_1.0.0_all/usr/bin/otp_voip_*
 
