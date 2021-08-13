1775883cd71b8c27925c181d41bc9be3cc18fb45
8or5q <76275385+8or5q@users.noreply.github.com>
2021-08-13 09:33:41 +0900

    last round of changes before moving to source package approach

---
+++ install_ubuntu_live.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+
+mkdir /tmp/nouveau
+cd /tmp/nouveau
+wget https://raw.github.com/envytools/firmware/master/extract_firmware.py
+wget http://us.download.nvidia.com/XFree86/Linux-x86/340.108/NVIDIA-Linux-x86-340.108.run
+sh NVIDIA-Linux-x86-340.108.run --extract-only
+python3 extract_firmware.py
+sudo mkdir /lib/firmware/nouveau
+sudo cp -d nv* vuc-* /lib/firmware/nouveau/
+
+sudo add-apt-repository -y universe multiverse
+#sudo apt update
+sudo apt install --no-install-recommends -y gdebi-core xz-utils
+wget https://github.com/8or5q/otp-voip/raw/master/build/ubuntu/otp-voip_1.0.2_all.deb
+sudo gdebi --non-interactive otp-voip_1.0.2_all.deb
+#sudo dpkg -i otp-voip_1.0.2_all.deb
+
+# vainfo should now show H264 support
+rm ~/.cache/gstreamer-1.0/registry.x86_64.bin
+# gst-inspect-1.0 libav should now show H264 support
+# gst-inspect-1.0 libav
